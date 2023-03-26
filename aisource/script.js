d73955e305617b944f0258afe3f8b6205cda6bbb
Sumon Ahemed <ahemedsumon03@gmail.com>
2023-03-27 00:22:11 +0600

    change script

--- aisource/script.js
+++ aisource/script.js
@@ -69,7 +69,7 @@ const handleSubmit = async (e) => {
     const messagediv = document.getElementById(uniqueid);
     loader(messagediv)
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://myai-wqui.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type':'application/json'
